396f7ff6306d268e41150c517eb15fcaa491e1a6
Manjeet Sharan <147024105+klaus2514@users.noreply.github.com>
2025-06-26 03:56:54 +0530

    Update Register.jsx

--- frontend/sports-safari/src/pages/Register.jsx
+++ frontend/sports-safari/src/pages/Register.jsx
@@ -14,7 +14,7 @@ function Register() {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/auth/signup', {
+      await axios.post('https://sportsafari-2.onrender.com/api/auth/signup', {
         name,
         email,
         password,
