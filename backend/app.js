16a2d655c264745cb8b17ad664cc11fe550dad2a
Manjeet Sharan <147024105+klaus2514@users.noreply.github.com>
2025-07-12 21:47:21 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://sportsafari-ten.vercel.app/'
+    'https://sportsafari-ten.vercel.app'
   ],
   credentials: true
 }));
